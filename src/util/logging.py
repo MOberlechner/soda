6799be8a9a2311c08128e8c207fddf4b8c8dbd10
unknown <matthias.oberlechner@tum.de>
2022-04-13 10:37:35 +0200

    restructuring

---
+++ src/util/logging.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from os.path import exists
+
+import pandas as pd
+
+
+def log_run(strategies, experiment: str, run: int, time_init, time_run, path: str):
+
+    # create new entry for each agent
+    rows = [
+        {
+            "experiment": experiment,
+            "run": run,
+            "agent": agent,
+            "utility": strategies[agent].utility[-1],
+            "utility_loss": strategies[agent].utility_loss[-1],
+            "iterations": len(strategies[agent].utility),
+            "iter/sec": round(len(strategies[agent].utility) / time_run, 2),
+            "time_init": round(time_init, 2),
+            "time_run": round(time_run, 2),
+            "timestamp": str(datetime.now())[:-7],
+        }
+        for agent in strategies
+    ]
+    # create DataFrame with entries
+    df = pd.DataFrame(rows)
+
+    if exists(path + "log.csv"):
+        # import existing dataframe
+        log = pd.read_csv(path + "log.csv")
+        log = pd.concat([log, df])
+    else:
+        log = df
+
+    # save data
+    log.to_csv(path + "log.csv", index=False)
---
+++ src/util/setting.py
@@ -0,0 +1,61 @@
+from src.game import Game
+from src.mechanism.all_pay import AllPay
+from src.mechanism.contest_game import ContestGame
+from src.mechanism.crowdsourcing import Crowdsourcing
+from src.strategy import Strategy
+
+
+def create_setting(setting: str, cfg):
+    """ Initialize mechanism, discretized game and strategies
+
+    Parameters
+    ----------
+    setting : str, specifies mechanism
+    cfg : config for experimt
+
+    Returns
+    -------
+    mechanism, game, strategy
+    """
+
+    if setting == "contest_game":
+        mechanism = ContestGame(
+            cfg.bidder,
+            cfg.o_space,
+            cfg.a_space,
+            cfg.param_prior,
+            cfg.csf,
+            cfg.param_csf,
+        )
+    elif setting == "all_pay":
+        mechanism = AllPay(
+            cfg.bidder,
+            cfg.o_space,
+            cfg.a_space,
+            cfg.param_prior,
+            cfg.util_setting,
+            cfg.param_util,
+        )
+
+    elif setting == "crowdsourcing":
+        mechanism = Crowdsourcing(
+            cfg.bidder,
+            cfg.o_space,
+            cfg.a_space,
+            cfg.param_prior,
+            cfg.price,
+            cfg.param_util,
+        )
+
+    else:
+        raise ValueError('Mechanism "' + setting + '" not available')
+
+    # create approximation game
+    game = Game(mechanism, cfg.n, cfg.m)
+
+    # create and initialize strategies
+    strategies = {}
+    for i in game.set_bidder:
+        strategies[i] = Strategy(i, game)
+
+    return mechanism, game, strategies
