1a9bf451f9f23cf446c4d823ef12010675ed58c9
Matthias Oberlechner <matthias.oberlechner@tum.de>
2022-05-06 07:02:45 +0200

    extended get_util for multi-dim action space

--- src/game.py
+++ src/game.py
@@ -28,9 +28,9 @@ class Game:
         self.n_bidder = mechanism.n_bidder
         self.n = n
         self.m = m
-        self.private_values = (
-            mechanism.private_values
-        )  # we distinguish between private and common values
+
+        # we distinguish between private and common values
+        self.private_values = mechanism.private_values
 
         # discrete action and observation space
         self.o_discr = {
@@ -41,29 +41,65 @@ class Game:
             i: discr_spaces(mechanism.a_space[i], m, midpoint=False)
             for i in self.set_bidder
         }
+        # dimension of spaces
+        self.dim_o = (
+            1
+            if len(self.o_discr[self.bidder[0]].shape) == 1
+            else self.o_discr[self.bidder[0]].shape[0]
+        )
+        self.dim_a = (
+            1
+            if len(self.a_discr[self.bidder[0]].shape) == 1
+            else self.a_discr[self.bidder[0]].shape[0]
+        )
 
         # marginal prior for bidder
         self.prior = {i: self.get_prior(mechanism, i) for i in self.set_bidder}
+
         self.weights = self.get_weights(mechanism)
         self.utility = {}
 
     def get_utility(self, mechanism):
 
         for i in self.set_bidder:
-            idx = self.bidder.index(i)
 
-            # create all possible bids (action space is one-dimensional)
-            bids = np.array(
-                [
-                    np.array(
-                        [
-                            self.a_discr[self.bidder[k]][j[k]]
-                            for j in product(range(self.m), repeat=self.n_bidder)
-                        ]
-                    )
-                    for k in range(self.n_bidder)
-                ]
-            )
+            idx = self.bidder.index(i)
+            if self.dim_a == 1:
+                # create all possible bids: 1-dim action space
+                bids = np.array(
+                    [
+                        np.array(
+                            [
+                                self.a_discr[self.bidder[k]][j[k]]
+                                for j in product(range(self.m), repeat=self.n_bidder)
+                            ]
+                        )
+                        for k in range(self.n_bidder)
+                    ]
+                )
+            else:
+                # create all possible bids: multi-dim action space
+                bids = np.array(
+                    [
+                        np.array(
+                            [
+                                np.array(
+                                    [
+                                        self.a_discr[self.bidder[i]][k][
+                                            j[self.dim_a * i + k]
+                                        ]
+                                        for j in product(
+                                            range(self.m),
+                                            repeat=self.n_bidder * self.dim_a,
+                                        )
+                                    ]
+                                )
+                                for k in range(self.dim_a)
+                            ]
+                        )
+                        for i in range(self.n_bidder)
+                    ]
+                )
 
             if self.private_values:
                 # valuation only depends on own observation
@@ -71,10 +107,15 @@ class Game:
                 self.utility[i] = (
                     mechanism.utility(valuations, bids, idx)
                     .transpose()
-                    .reshape(tuple([self.m] * self.n_bidder + [self.n]))
+                    .reshape(
+                        tuple(
+                            [self.m] * (self.dim_a * self.n_bidder)
+                            + [self.n] * self.dim_o
+                        )
+                    )
                 )
 
-            elif (not self.private_values) and (self.weights is not None):
+            else:
                 # affiliated values model with correlated observations and common value
                 valuations = self.o_discr[i]
                 self.utility[i] = (
@@ -82,8 +123,6 @@ class Game:
                     .transpose()
                     .reshape(tuple([self.m] * self.n_bidder + [self.n] * self.n_bidder))
                 )
-            else:
-                raise NotImplementedError
 
     def get_prior(self, mechanism, agent):
         p = marginal_prior_pdf(mechanism, self.o_discr[agent], agent)
@@ -98,47 +137,50 @@ class Game:
 # -------------------------------------------------------------------------------------------------------------------- #
 
 
-def discr_spaces(interval: List, n: int, midpoint: bool):
-    """
-    Parameters
-    ----------
-    interval : List, contains lists with observation intervals for each bidder
-    n : number of discretization points
-    midpoint : take midpoints of discretization
+def discr_spaces(interval: List, n_discrete: int, midpoint: bool) -> np.ndarray:
+    """Discretize Spaces (possibly multidimensional)
 
-    Returns
-    -------
-    discrete_space: dict, containing array with discretized space for each bidder
-    dim : int, dimension of respective space
+    Args:
+        interval (List): contains lists with space intervals for each bidder
+        n_discrete (int):  number of discretization points
+        midpoint (bool): take midpoints of discretization
+
+    Returns:
+        np.ndarray: discretized space, shape (dimension, n_discrete)
     """
 
     # check dimension of observation space (if more dimensional, interval is nested list)
     if len(np.array(interval).shape) > 1:
         return np.array(
-            [discr_interval(interv[0], interv[1], n, midpoint) for interv in interval]
+            [
+                discr_interval(interv[0], interv[1], n_discrete, midpoint)
+                for interv in interval
+            ]
         )
     else:
-        return discr_interval(interval[0], interval[1], n, midpoint)
+        return discr_interval(interval[0], interval[1], n_discrete, midpoint)
 
 
-def discr_interval(a: float, b: float, n: int, midpoint: bool):
-    """
-    Discretize interval [a,b] using n points
+def discr_interval(
+    lower_bound: float, upper_bound: float, n_discrete: int, midpoint: bool
+) -> np.ndarray:
+    """Discretize interval
 
-    Parameters
-    ----------
-    a : float, lower bound interval
-    b : float, upper bound interval
-    n : int, number of discretization points
-    midpoint : bool, if True returns midpoints of n subintervals, else a, b and n-2 points in between
+    Args:
+        lower_bound (float): lower bound of interval
+        b (float): upper bound of interval
+        n_discrete (int): number of discretization points
+        midpoint (bool): if True returns midpoints of n subintervals, else a, b and n-2 points in between
 
-    Returns
-    -------
-    array(n), discretized interval
+    Returns:
+        np.ndarray: discretized interval
     """
     if midpoint:
-        # split interval in n equally sized subintervals and take midpoints (observation/valuation)
-        return a + (0.5 + np.arange(n)) * (b - a) / n
+        return (
+            lower_bound
+            + (0.5 + np.arange(n_discrete)) * (upper_bound - lower_bound) / n_discrete
+        )
     else:
-        # take minimal and maximal value of interval and distribute n-2 remaining points equally in between (action)
-        return a + (np.arange(n)) * (b - a) / (n - 1)
+        return lower_bound + (np.arange(n_discrete)) * (upper_bound - lower_bound) / (
+            n_discrete - 1
+        )
