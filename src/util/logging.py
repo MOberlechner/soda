ac6145dcca6786ef7e8c78bb98b35962d5bbc463
Matthias Oberlechner <matthias.oberlechner@tum.de>
2022-08-26 06:36:43 +0200

    additional metrics and new method to aggregated results from simulations

--- src/util/logging.py
+++ src/util/logging.py
@@ -5,7 +5,15 @@ import pandas as pd
 
 
 def log_run(
-    strategies, experiment: str, setting: str, run: int, time_init, time_run, path: str
+    strategies,
+    cfg_learner,
+    experiment: str,
+    setting: str,
+    run: int,
+    time_init,
+    time_run,
+    conv: bool,
+    path: str,
 ):
 
     # create new entry for each agent
@@ -18,6 +26,7 @@ def log_run(
             "utility": strategies[agent].utility[-1],
             "utility_loss": strategies[agent].utility_loss[-1],
             "iterations": len(strategies[agent].utility),
+            "convergence": strategies[agent].utility_loss[-1] < cfg_learner.tol,
             "iter/sec": round(len(strategies[agent].utility) / time_run, 2),
             "time_init": round(time_init, 2),
             "time_run": round(time_run, 2),
@@ -42,6 +51,7 @@ def log_run(
 def log_sim(
     strategies, experiment: str, setting: str, run: int, tag, values, path: str
 ):
+    """ """
 
     # create new entry for each agent
     rows = [
@@ -68,3 +78,34 @@ def log_sim(
 
     # save data
     log.to_csv(path + "log_sim.csv", index=False)
+
+
+def agg_log_sim(path, decimal=4):
+    """
+    Aggregate results from log_sim: mean and std
+    """
+
+    # import log
+    df = pd.read_csv(path + "log_sim.csv").drop(columns=["timestamp"])
+
+    # aggregate over runs
+    metrics = df["tag"].unique()
+    df = pd.pivot(
+        df,
+        index=["experiment", "mechanism", "run", "agent"],
+        columns="tag",
+        values="value",
+    ).reset_index()
+    df1 = df.groupby(["experiment", "mechanism", "agent"]).agg(
+        {tag: "mean" for tag in metrics}
+    )
+    df1.columns = [c + "_mean" for c in df1.columns]
+    df2 = df.groupby(["experiment", "mechanism", "agent"]).agg(
+        {tag: "std" for tag in metrics}
+    )
+    df2.columns = [c + "_std" for c in df1.columns]
+    df = df1.merge(df2, how="outer", left_index=True, right_index=True)
+    df = df.reindex(sorted(df.columns), axis=1).round(4)
+
+    # save aggregate log file
+    df.to_csv(path + "log_sim_agg.csv", index=False)
