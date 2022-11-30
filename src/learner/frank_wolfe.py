f51c94f2b9583c0e0c168d9718a3722e7cc91cfd
Matthias Oberlechner <matthias.oberlechner@tum.de>
2022-11-30 14:55:34 +0100

    new method for frank_wolfe

--- src/learner/frank_wolfe.py
+++ src/learner/frank_wolfe.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from .learner import Learner
+from src.learner.learner import Learner
 
 
 class FrankWolfe(Learner):
@@ -10,7 +10,9 @@ class FrankWolfe(Learner):
         method (str): specify learning algorithm
         max_iter (int): maximal number of iterations
         tol (float): stopping criterion (relative utility loss)
-        stop_criterion (str): specify stopping criterion. Defaults to "util_loss"
+        stop_criterion (str): specify stopping criterion. Defaults to 'util_loss'
+        method (str, optional): Choose between standard and online version of FW. Defaults to 'standard'.
+        param_step (float, optional): parameter for step size for online version of FW
     """
 
     def __init__(
@@ -18,32 +20,43 @@ class FrankWolfe(Learner):
         max_iter: int,
         tol: float,
         stop_criterion: str,
+        method: str = "standard",
+        param_step: float = -0.2,
     ):
         super().__init__(max_iter, tol, stop_criterion)
         self.learner = "frank_wolfe"
+        self.method = method
+        self.param_step = param_step
 
     def update_strategy(self, strategy, gradient: np.ndarray, t: int) -> None:
-        """
-        Update strategy: Frank-Wolfe Algorithm (or Best Response dynamic)
+        """Update strategy according to Frank-Wolfe (FW)
 
-        Parameters
-        ----------
-        strategy : class Strategy
-        gradient : np.ndarray,
-        stepsize : np.ndarray, step size
+        Args:
+            strategy: Strategy
+            gradient (np.ndarray): current gradient
+            t (int): current iteration
         """
-        # solution of minimization problem
-        br = strategy.best_response(gradient)
-        # update step
-        strategy.x = self.update_step(strategy.x, br, t)
+        if self.method == "standard":
+            br = strategy.best_response(gradient)
+            step = 2 / (2 + t)
 
-    def update_step(self, x: np.ndarray, x_opt: np.ndarray, t):
+        elif self.method == "online":
+            strategy.y += gradient
+            gradient_agg = strategy.y / (t + 1)
+            br = strategy.best_response(gradient_agg)
+            step = (t + 1) ** self.param_step
+
+        else:
+            raise ValueError(f"'{self.method}'-Version of Frank-Wolfe unkonwn")
+
+        strategy.x = self.update_step(strategy.x, br, step)
+
+    def update_step(self, x: np.ndarray, x_opt: np.ndarray, step: float) -> np.ndarray:
         """Update Step for Frank-Wolfe
 
         Args:
             x (np.ndarray): current iterate
             x_opt (np.ndarray): solution of linear program
-            stepsize (int): currrent iteration (starts at 0)
+            step (float): step towards x_opt
         """
-        gamma = 2 / (t + 2)
-        return (1 - gamma) * x + gamma * x_opt
+        return (1 - step) * x + step * x_opt
