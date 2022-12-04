1166a9502bc11a1292f6eb001413ade5a72554c9
Matthias Oberlechner <matthias.oberlechner@tum.de>
2022-12-04 08:26:36 +0100

    included new version of frank_wolfe

--- src/learner/frank_wolfe.py
+++ src/learner/frank_wolfe.py
@@ -20,13 +20,17 @@ class FrankWolfe(Learner):
         max_iter: int,
         tol: float,
         stop_criterion: str,
-        method: str = "standard",
-        param_step: float = -0.2,
+        method: str,
+        steprule_bool: bool,
+        eta: float,
+        beta: float,
     ):
         super().__init__(max_iter, tol, stop_criterion)
         self.learner = "frank_wolfe"
         self.method = method
-        self.param_step = param_step
+        self.steprule_bool = steprule_bool
+        self.eta = eta
+        self.beta: beta
 
     def update_strategy(self, strategy, gradient: np.ndarray, t: int) -> None:
         """Update strategy according to Frank-Wolfe (FW)
@@ -38,18 +42,18 @@ class FrankWolfe(Learner):
         """
         if self.method == "standard":
             br = strategy.best_response(gradient)
-            step = 2 / (2 + t)
+            eta_t = self.step_rule(t)
 
         elif self.method == "online":
             strategy.y += gradient
             gradient_agg = strategy.y / (t + 1)
             br = strategy.best_response(gradient_agg)
-            step = (t + 1) ** self.param_step
+            eta_t = self.step_rule(t)
 
         else:
             raise ValueError(f"'{self.method}'-Version of Frank-Wolfe unkonwn")
 
-        strategy.x = self.update_step(strategy.x, br, step)
+        strategy.x = self.update_step(strategy.x, br, eta_t)
 
     def update_step(self, x: np.ndarray, x_opt: np.ndarray, step: float) -> np.ndarray:
         """Update Step for Frank-Wolfe
@@ -60,3 +64,26 @@ class FrankWolfe(Learner):
             step (float): step towards x_opt
         """
         return (1 - step) * x + step * x_opt
+
+    def step_rule(self, t: int) -> np.ndarray:
+        """Compute step size:
+        if step_rule is True: step sizes are not summable, but square-summable
+        if step_rule is False: standard rule for Frank-Wolfe (2/t+2)
+
+        Args:
+            t (int): current iteration
+            grad (np.ndarray): gradient, necessary if steprule_bool is False (heuristic)
+            dim_o (int): dimension of type space
+            dim_a (int): dimension of action space
+
+        Returns:
+            np.ndarray: step size eta (either scaler or for each valuation)
+        """
+
+        if self.steprule_bool:
+            # decreasing step rule (Mertikopoulos&Zhou)
+            return np.array(self.eta * 1 / (t + 1) ** self.beta)
+
+        else:
+            # standard FW
+            return 2 / (t + 2)
