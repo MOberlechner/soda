eac58931d30b16e7cba33b2e9ca28cbc9f28aa30
Matthias Oberlechner <matthias.oberlechner@tum.de>
2023-01-10 17:56:23 +0100

    update config class

--- src/util/config.py
+++ src/util/config.py
@@ -15,6 +15,7 @@ from src.mechanism.contest_game import ContestGame
 from src.mechanism.crowdsourcing import Crowdsourcing
 from src.mechanism.double_auction import DoubleAuction
 from src.mechanism.llg_auction import LLGAuction
+from src.mechanism.multi_unit import MultiUnitAuction
 from src.mechanism.single_item import SingleItemAuction
 from src.mechanism.split_award import SplitAwardAuction
 from src.strategy import Strategy
@@ -30,14 +31,62 @@ class Config:
     ):
         """Initialize Config Class"""
 
+    def create_experiment(setting: str, experiment: str, learner: str):
+        """create mechanism, game, strategies and learner according to config file
+
+        Args:
+            setting (str): mechanism
+            experiment (str): name of config file in mechanism directory
+            learner (str): name of config file for learner in mechanism/learner directory
+        """
+        pass
+
+    def create_config_game(
+        self,
+        setting: str,
+        bidder: list,
+        o_space: dict,
+        a_space: dict,
+        param_prior: dict,
+        param_util: dict,
+        n: int,
+        m: int,
+    ) -> None:
+        """Create config file to create mechanism and game
+
+        Args:
+            setting (str): mechanism
+            bidder (list): list of agents
+            o_space (dict): contains intervals for observation space for bidders
+            a_space (dict): contains intervals for action space for bidders
+            param_prior (dict): contains parameters for prior
+            param_util (dict): contains parameters for utility function
+            n (int): number discretization points observation intervals
+            m (int): number discretization points action intervals
+        """
+        self.config_game = {
+            "mechanism": setting,
+            "bidder": bidder,
+            "o_space": o_space,
+            "a_space": a_space,
+            "param_util": param_util,
+            "param_prior": param_prior,
+            "n": n,
+            "m": m,
+        }
+
     def create_strategies(
         self, game, init_method: str = "random", param_init: dict = {}
     ) -> dict:
-        """create dict with all strategies
+        """Create strategy profile
 
         Args:
             game (Game): approximation game
-            init_method:
+            init_method (str, optional): initialization method for strategy. Defaults to "random".
+            param_init (dict, optional): Parameter for initialization method. Defaults to {}.
+
+        Returns:
+            dict: contains strategy for each unique bidder
         """
         strategies = {}
         for i in game.set_bidder:
@@ -60,12 +109,6 @@ class Config:
 
     def create_mechanism(
         self,
-        setting: str,
-        bidder: list,
-        o_space: dict,
-        a_space: dict,
-        param_prior: dict,
-        param_util: dict,
     ):
         """_summary_
 
@@ -83,83 +126,96 @@ class Config:
         Returns:
             Mechanism
         """
+        if not hasattr(self, "config_game"):
+            raise ValueError("configuration for mechanism/game not created")
+
+        setting = self.config_game["mechanism"]
 
         if setting == "single_item":
             mechanism = SingleItemAuction(
-                bidder,
-                o_space,
-                a_space,
-                param_prior,
-                param_util,
+                self.config_game["bidder"],
+                self.config_game["o_space"],
+                self.config_game["a_space"],
+                self.config_game["param_prior"],
+                self.config_game["param_util"],
             )
         elif setting == "llg_auction":
             mechanism = LLGAuction(
-                bidder,
-                o_space,
-                a_space,
-                param_prior,
-                param_util,
+                self.config_game["bidder"],
+                self.config_game["o_space"],
+                self.config_game["a_space"],
+                self.config_game["param_prior"],
+                self.config_game["param_util"],
             )
-        elif self.setting == "contest_game":
+        elif setting == "contest_game":
             mechanism = ContestGame(
-                bidder,
-                o_space,
-                a_space,
-                param_prior,
-                param_util,
+                self.config_game["bidder"],
+                self.config_game["o_space"],
+                self.config_game["a_space"],
+                self.config_game["param_prior"],
+                self.config_game["param_util"],
             )
-        elif self.setting == "all_pay":
+        elif setting == "all_pay":
             mechanism = AllPay(
-                bidder,
-                o_space,
-                a_space,
-                param_prior,
-                param_util,
+                self.config_game["bidder"],
+                self.config_game["o_space"],
+                self.config_game["a_space"],
+                self.config_game["param_prior"],
+                self.config_game["param_util"],
             )
-        elif self.setting == "crowdsourcing":
+        elif setting == "crowdsourcing":
             mechanism = Crowdsourcing(
-                bidder,
-                o_space,
-                a_space,
-                param_prior,
-                param_util,
+                self.config_game["bidder"],
+                self.config_game["o_space"],
+                self.config_game["a_space"],
+                self.config_game["param_prior"],
+                self.config_game["param_util"],
             )
-        elif self.setting == "split_award":
+        elif setting == "split_award":
             mechanism = SplitAwardAuction(
-                bidder,
-                o_space,
-                a_space,
-                param_prior,
-                param_util,
+                self.config_game["bidder"],
+                self.config_game["o_space"],
+                self.config_game["a_space"],
+                self.config_game["param_prior"],
+                self.config_game["param_util"],
             )
-        elif self.setting == "double_auction":
+        elif setting == "double_auction":
             mechanism = DoubleAuction(
-                bidder,
-                o_space,
-                a_space,
-                param_prior,
-                param_util,
+                self.config_game["bidder"],
+                self.config_game["o_space"],
+                self.config_game["a_space"],
+                self.config_game["param_prior"],
+                self.config_game["param_util"],
             )
-        elif self.setting == "bertrand_pricing":
+        elif setting == "bertrand_pricing":
             mechanism = BertrandPricing(
-                bidder,
-                o_space,
-                a_space,
-                param_prior,
-                param_util,
+                self.config_game["bidder"],
+                self.config_game["o_space"],
+                self.config_game["a_space"],
+                self.config_game["param_prior"],
+                self.config_game["param_util"],
+            )
+        elif setting == "multi_unit":
+            mechanism = MultiUnitAuction(
+                self.config_game["bidder"],
+                self.config_game["o_space"],
+                self.config_game["a_space"],
+                self.config_game["param_prior"],
+                self.config_game["param_util"],
             )
         else:
             raise ValueError('Mechanism "{}" not available'.format(setting))
-
         return mechanism
 
     def create_learner(
         self,
+        name: str,
         max_iter: int,
         tol: float,
-        stop_criteration: str,
+        stop_criterion: str,
         regularizer: str = None,
         mirror_map: str = None,
+        method: str = None,
         step_rule: bool = True,
         eta: float = None,
         beta: float = None,
@@ -176,7 +232,55 @@ class Config:
             eta (float, optional): _description_. Defaults to None.
             beta (float, optional): _description_. Defaults to None.
         """
-        # todo
+        if name == "soda":
+            return SODA(
+                max_iter,
+                tol,
+                stop_criterion,
+                regularizer,
+                step_rule,
+                eta,
+                beta,
+            )
+
+        elif name == "soma":
+            return SOMA(
+                max_iter,
+                tol,
+                stop_criterion,
+                mirror_map,
+                step_rule,
+                eta,
+                beta,
+            )
+
+        elif name == "frank_wolfe":
+            return FrankWolfe(
+                max_iter,
+                tol,
+                stop_criterion,
+                method,
+                step_rule,
+                eta,
+                beta,
+            )
+
+        elif name == "fictitious_play":
+            return FictitiousPlay(
+                max_iter,
+                tol,
+                stop_criterion,
+            )
+
+        elif name == "best_response":
+            return BestResponse(
+                max_iter,
+                tol,
+                stop_criterion,
+            )
+
+        else:
+            raise ValueError(f"Learner {name} unknown.")
 
     def get_path(self, path):
         """get path into config directory
