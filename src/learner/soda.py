b439a0484fde8f5cb88b078127eacecd3be86c80
Matthias Oberlechner <matthias.oberlechner@tum.de>
2023-02-01 13:14:05 +0100

    created learner_util to avoid duplicated code

--- src/learner/soda.py
+++ src/learner/soda.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from .learner import Learner
+from src.learner.learner import Learner
+from src.util import learner_util
 
 
 class SODA(Learner):
@@ -91,7 +92,7 @@ class SODA(Learner):
             tuple (np.ndarray): next dual and primal iterate
         """
         y += eta_t * grad
-        x = self.projection(y, prior)
+        x = learner_util.project_euclidean(y, prior)
         return x, y
 
     def update_step_entropic(
@@ -121,54 +122,6 @@ class SODA(Learner):
             list(prior.shape) + [1] * dim_a
         )
         return 1 / xc_exp_sum * prior.reshape(list(prior.shape) + [1] * dim_a) * xc_exp
-
-    def projection(self, x: np.ndarray, prior: np.ndarray) -> np.ndarray:
-        """Projection w.r.t. Euclidean distance
-        each row x[i] is projected to the probability simplex scaled by prior[i]
-        Algorithm based on https://arxiv.org/pdf/1309.1541.pdf
-
-        Args:
-            x (np.ndarry): 2-dim array
-            prior (np.ndarray): marginal prior
-
-        Returns:
-            np.ndarray: projection of x
-        """
-        bool_split_award = False
-        if len(x.shape) > 2:
-            # Split Award (1-dim observation space, 2-dim action space)
-            if (len(prior.shape) == 1) & (len(x.shape) == 3):
-                bool_split_award = True
-                n, m1, m2 = x.shape
-                x = x.reshape(n, m1 * m2)
-            else:
-                raise NotImplementedError(
-                    "Projection only implemented for 1-dim action space and valuation space"
-                )
-
-        n, m = x.shape
-
-        assert n == len(prior), "dimensions of strategy and prior not compatible"
-
-        # sort
-        x_sort = -np.sort(-x, axis=1)
-        x_cumsum = x_sort.cumsum(axis=1)
-        # find rho
-        rho = np.array(
-            (x_sort + (prior.reshape(n, 1) - x_cumsum) / np.arange(1, m + 1) > 0).sum(
-                axis=1
-            ),
-            dtype=int,
-        )
-        # define lambda
-        lamb = 1 / rho * (prior - x_cumsum[range(n), rho - 1])
-        x_proj = (x + np.repeat(lamb, m).reshape(n, m)).clip(min=0)
-
-        # Split Award (1-dim observation space, 2-dim action space)
-        if bool_split_award:
-            return x_proj.reshape(n, m1, m2)
-        else:
-            return x_proj
 
     def step_rule(self, t: int, grad: np.ndarray, dim_o: int, dim_a: int) -> np.ndarray:
         """Compute step size:
