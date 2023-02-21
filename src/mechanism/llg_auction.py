4c3c28b587dc1715f268741e4e61afa60449ab42
Matthias Oberlechner <matthias.oberlechner@tum.de>
2023-02-21 19:15:11 +0100

    include first-price payment rule and additional metrics to mechanism

--- src/mechanism/llg_auction.py
+++ src/mechanism/llg_auction.py
@@ -11,6 +11,21 @@ from .mechanism import Mechanism
 
 
 class LLGAuction(Mechanism):
+    """Combinatorial Auction: LLG-Model
+
+    Parameter Mechanism
+        bidder, o_space, a_space - standard input for all mechanism (see class Mechanism)
+
+    Parameter Prior (param_prior)
+        distribution
+        corr            float, correlation of observations/valuation of local bidders
+
+    Parameter Utility (param_util)
+        tiebreaking     str: specifies tiebreaking rule: random, lose or local
+        payment_rule    str, nearest_vcg, nearest_bid, nearest_zero or first_price
+
+    """
+
     def __init__(
         self,
         bidder: List[str],
@@ -28,18 +43,16 @@ class LLGAuction(Mechanism):
         self.tie_breaking = param_util["tie_breaking"]
         self.gamma = param_prior["corr"]
 
-    def utility(self, obs: np.ndarray, bids: np.ndarray, idx: int):
-        """Payoff function for first price sealed bid auctons
+    def utility(self, obs: np.ndarray, bids: np.ndarray, idx: int) -> np.ndarray:
+        """_summary_
 
-        Parameters
-        ----------
-        obs : observation/valuation of bidder (idx)
-        bids : array with bid profiles
-        idx : index of bidder to consider
+        Args:
+            obs (np.ndarray): observation/valuation of agent
+            bids (np.ndarray): bid profile
+            idx (int): index/position of agent
 
-        Returns
-        -------
-        utility for the LLG-Auction under different bidder-optimal core-selecting rules
+        Returns:
+            np.ndarray: payoff vector for agent (idx)
         """
 
         self.test_input_utility(obs, bids, idx)
@@ -85,14 +98,11 @@ class LLGAuction(Mechanism):
         Returns:
             np.ndarray: payment vector
         """
-        # global bidder
-        if idx == 2:
-            payment = bids[:2].sum(axis=0)
 
-        # local bidder
-        else:
-            # Nearest-Zero (Proxy Rule)
-            if self.payment_rule == "NZ":
+        if self.payment_rule == "nearest_zero":
+            if idx == 2:  # global bidder
+                payment = bids[:2].sum(axis=0)
+            else:  # local bidder
                 case_a = bids[2] <= 2 * bids[:2].min(axis=0)
                 payment = case_a * 0.5 * bids[2] + (1 - case_a) * np.where(
                     bids[idx] == bids[:2].min(axis=0),
@@ -100,8 +110,10 @@ class LLGAuction(Mechanism):
                     bids[2] - bids[:2].min(axis=0),
                 )
 
-            # Nearest-VCG Rule
-            elif self.payment_rule == "NVCG":
+        elif self.payment_rule == "nearest_vcg":
+            if idx == 2:  # global bidder
+                payment = bids[:2].sum(axis=0)
+            else:  # local bidders
                 vcg_payments = {
                     0: -bids[1] + np.maximum(bids[1], bids[2]),
                     1: -bids[0] + np.maximum(bids[0], bids[2]),
@@ -109,16 +121,21 @@ class LLGAuction(Mechanism):
                 delta = 0.5 * (bids[2] - vcg_payments[0] - vcg_payments[1])
                 payment = vcg_payments[idx] + delta
 
-            # Nearest-Bid Rule
-            elif self.payment_rule == "NB":
+        elif self.payment_rule == "nearest_bid":
+            if idx == 2:  # global bidder
+                payment = bids[:2].sum(axis=0)
+            else:  # local bidders
                 case_a = bids[2] <= bids[:2].max(axis=0) - bids[:2].min(axis=0)
                 delta = 0.5 * (bids[:2].sum(axis=0) - bids[2])
                 payment = case_a * np.where(
                     bids[idx] == bids[:2].max(axis=0), bids[2], 0
                 ) + (1 - case_a) * (bids[idx] - delta)
 
-            else:
-                raise ValueError("payment rule unknown")
+        elif self.payment_rule == "first_price":
+            payment = bids[idx]
+
+        else:
+            raise ValueError("payment rule unknown")
 
         return payment * np.where(allocation > 0, 1.0, 0.0)
 
@@ -172,6 +189,69 @@ class LLGAuction(Mechanism):
                 ]
             )
 
+    # --------------------------------- methods to compute metrics --------------------------------- #
+
+    def get_metrics(
+        self, agent: str, obs_profile: np.ndarray, bid_profile: np.ndarray
+    ) -> tuple:
+        """compute all metrics relevant for single-item auction
+        this includes standard metrics (l2, util_loss) + revenue
+        """
+        metrics, values = self.get_standard_metrics(agent, obs_profile, bid_profile)
+        metrics += ["revenue"]
+        values += [self.compute_revenue(obs_profile, bid_profile)]
+        metrics += ["efficiency"]
+        values += [self.compute_efficiency(obs_profile, bid_profile)]
+        return metrics, values
+
+    def compute_revenue(
+        self, obs_profile: np.ndarray, bid_profile: np.ndarray
+    ) -> float:
+        """Compute expected revenue for LLG Model
+
+        Args:
+            bid_profile (np.ndarray): bid profile
+
+        Returns:
+            float: approximated expected revenue
+        """
+        allocations = np.array(
+            [self.get_allocation(bid_profile, i) for i in range(self.n_bidder)]
+        )
+        payments = np.array(
+            [
+                self.get_payment(bid_profile, allocations[i], i)
+                for i in range(self.n_bidder)
+            ]
+        )
+        welfare = (allocations * obs_profile).sum(axis=0)
+        revenue = (allocations * payments).sum(axis=0)
+        return revenue.mean() / welfare.mean()
+
+    def compute_efficiency(
+        self, obs_profile: np.ndarray, bid_profile: np.ndarray
+    ) -> float:
+        """_summary_
+
+        Args:
+            obs_profile (np.ndarray): valuation profiles
+            bid_profile (np.ndarray): bid profiles
+
+        Returns:
+            float: efficiency (percentage of possible total welfare)
+        """
+        allocations_truthful = np.array(
+            [self.get_allocation(obs_profile, i) for i in range(self.n_bidder)]
+        )
+        allocations_bids = np.array(
+            [self.get_allocation(bid_profile, i) for i in range(self.n_bidder)]
+        )
+        welfare_truthful = (allocations_truthful * obs_profile).sum(axis=0)
+        welfare_bids = (allocations_bids * obs_profile).sum(axis=0)
+        return welfare_bids.mean() / welfare_truthful.mean()
+
+    # ------------------------------- methods to determin equilibria ------------------------------- #
+
     def get_bne(self, agent: str, obs: np.ndarray) -> np.ndarray:
         """
         Returns BNE for different payment rulse (assuming observations and actions for local bidders are [0,1])
@@ -183,10 +263,20 @@ class LLGAuction(Mechanism):
         Returns:
             np.ndarray: equilibrium bid for each observation
         """
-        if agent == "G":
-            return obs
-        elif agent == "L":
-            if self.payment_rule == "NVCG":
+        bnes = [
+            self.bne_nearest_vcg(agent, obs),
+            self.bne_nearest_nz(agent, obs),
+            self.bne_nearest_nb(agent, obs),
+        ]
+        for bne in bnes:
+            if bne is not None:
+                return bne
+        return None
+
+    def bne_nearest_vcg(self, agent: str, obs: np.ndarray):
+        """BNE for LLG Auction with Nearest-VCG payment rule"""
+        if (self.payment_rule == "nearest_vcg") & (self.prior == "uniform"):
+            if agent == "L":
                 if self.gamma == 1:
                     return 2 / 3 * obs
                 else:
@@ -196,14 +286,32 @@ class LLGAuction(Mechanism):
                     return np.maximum(
                         np.zeros(len(obs)), 2 / (2 + self.gamma) * (obs - obs_star)
                     )
+            elif agent == "G":
+                return obs
+            else:
+                raise ValueError("BNE only for local (L) or global (G) bidder.")
+        else:
+            return None
 
-            elif self.payment_rule == "NZ":
+    def bne_nearest_nz(self, agent: str, obs: np.ndarray):
+        """BNE for LLG Auction with Nearest-Zero payment rule"""
+        if (self.payment_rule == "nearest_zero") & (self.prior == "uniform"):
+            if agent == "L":
                 return np.maximum(
                     np.zeros(len(obs)),
                     np.log(self.gamma + (1 - self.gamma) * obs) / (1 - self.gamma) + 1,
                 )
+            elif agent == "G":
+                return obs
+            else:
+                raise ValueError("BNE only for local (L) or global (G) bidder.")
+        else:
+            return None
 
-            elif self.payment_rule == "NB":
+    def bne_nearest_nb(self, agent: str, obs: np.ndarray):
+        """BNE for LLG Auction with Nearest-Bid payment rule"""
+        if (self.payment_rule == "nearest_bid") & (self.prior == "uniform"):
+            if agent == "L":
                 if self.gamma == 1:
                     return 1 / 2 * obs
                 else:
@@ -212,11 +320,12 @@ class LLGAuction(Mechanism):
                         / (1 - self.gamma)
                         * (np.log(2) - np.log(2 - (1 - self.gamma) * obs))
                     )
-
+            elif agent == "G":
+                return obs
             else:
-                raise ValueError("BNE not available: payment rule unknown")
+                raise ValueError("BNE only for local (L) or global (G) bidder.")
         else:
-            raise ValueError("BNE only for local (L) or global (G) bidder.")
+            return None
 
     def check_param(self):
         """
@@ -229,7 +338,12 @@ class LLGAuction(Mechanism):
 
         if "payment_rule" not in self.param_util:
             raise ValueError("specify payment rule")
-        elif self.param_util["payment_rule"] not in ["NVCG", "NZ", "NB"]:
+        elif self.param_util["payment_rule"] not in [
+            "nearest_vcg",
+            "nearest_zero",
+            "nearest_bid",
+            "first_price",
+        ]:
             raise ValueError("payment rule is not available")
 
         if self.bidder[2] != "G":
