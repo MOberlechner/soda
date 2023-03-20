4157d45433a576127e47e2b031d4b55bb38c6578
Matthias Oberlechner <matthias.oberlechner@tum.de>
2023-03-20 13:41:59 +0100

    refactored config test

--- tests/config_test.py
+++ tests/config_test.py
@@ -10,77 +10,50 @@ from src.game import Game
 from src.learner.learner import Learner
 from src.util.config import Config
 
-""" ------------------------ Test config files from soda/configs --------------------------- """
-# get all config files form soda/configs for games and learners
+""" Add paths to config directories """
+
+paths_to_config = [
+    "configs/",
+    "experiment/soda_or/configs/",
+]
+
+
+""" Test config files from specified paths"""
 path_into_project = os.getcwd().split("soda")[0] + "soda/"
-path_to_config = "configs/"
-testdata_experiments, testdata_learner = [], []
-for mechanism_type in os.listdir(f"{path_into_project}{path_to_config}"):
-    for experiment in os.listdir(
-        f"{path_into_project}{path_to_config}{mechanism_type}/"
-    ):
-        if experiment != "learner":
-            testdata_experiments += [(mechanism_type, experiment.replace(".yaml", ""))]
+for path_config in paths_to_config:
 
-    for learn_alg in os.listdir(
-        f"{path_into_project}{path_to_config}{mechanism_type}/learner/"
-    ):
-        testdata_learner += [(mechanism_type, learn_alg.replace(".yaml", ""))]
+    # get all config files
+    testdata_experiments, testdata_learner = [], []
+    for mechanism_type in os.listdir(f"{path_into_project}{path_config}"):
+        for experiment in os.listdir(
+            f"{path_into_project}{path_config}{mechanism_type}/"
+        ):
+            if experiment != "learner":
+                testdata_experiments += [
+                    (mechanism_type, experiment.replace(".yaml", ""))
+                ]
 
+        for learn_alg in os.listdir(
+            f"{path_into_project}{path_config}{mechanism_type}/learner/"
+        ):
+            testdata_learner += [(mechanism_type, learn_alg.replace(".yaml", ""))]
 
-@pytest.mark.parametrize("mechanism_type, experiment", testdata_experiments)
-def test_config_games(mechanism_type, experiment):
-    config = Config()
-    config.get_path(path_to_config)
-    config.get_config_game(mechanism_type, experiment)
-    game = config.create_game()
+    # test config files for games
+    @pytest.mark.parametrize("mechanism_type, experiment", testdata_experiments)
+    def test_config_games(mechanism_type, experiment):
+        config = Config()
+        config.get_path(path_config)
+        config.get_config_game(mechanism_type, experiment)
+        game = config.create_game()
 
-    assert isinstance(game, Game)
+        assert isinstance(game, Game)
 
+    # test config files for learner
+    @pytest.mark.parametrize("mechanism_type, learn_alg", testdata_learner)
+    def test_config_learner(mechanism_type, learn_alg):
+        config = Config()
+        config.get_path(path_config)
+        config.get_config_learner(mechanism_type, learn_alg)
+        learner = config.create_learner()
 
-@pytest.mark.parametrize("mechanism_type, learn_alg", testdata_learner)
-def test_config_learner(mechanism_type, learn_alg):
-    config = Config()
-    config.get_path(path_to_config)
-    config.get_config_learner(mechanism_type, learn_alg)
-    learner = config.create_learner()
-
-    assert isinstance(learner, Learner)
-
-
-""" -------------- Test config files from soda/experiments/soda_or/configs ---------------- """
-# get all config files form soda/configs for games and learners
-path_into_project = os.getcwd().split("soda")[0] + "soda/"
-path_to_config = "experiment/soda_or/configs/"
-testdata_experiments, testdata_learner = [], []
-for mechanism_type in os.listdir(f"{path_into_project}{path_to_config}"):
-    for experiment in os.listdir(
-        f"{path_into_project}{path_to_config}{mechanism_type}/"
-    ):
-        if experiment != "learner":
-            testdata_experiments += [(mechanism_type, experiment.replace(".yaml", ""))]
-
-    for learn_alg in os.listdir(
-        f"{path_into_project}{path_to_config}{mechanism_type}/learner/"
-    ):
-        testdata_learner += [(mechanism_type, learn_alg.replace(".yaml", ""))]
-
-
-@pytest.mark.parametrize("mechanism_type, experiment", testdata_experiments)
-def test_config_games(mechanism_type, experiment):
-    config = Config()
-    config.get_path(path_to_config)
-    config.get_config_game(mechanism_type, experiment)
-    game = config.create_game()
-
-    assert isinstance(game, Game)
-
-
-@pytest.mark.parametrize("mechanism_type, learn_alg", testdata_learner)
-def test_config_learner(mechanism_type, learn_alg):
-    config = Config()
-    config.get_path(path_to_config)
-    config.get_config_learner(mechanism_type, learn_alg)
-    learner = config.create_learner()
-
-    assert isinstance(learner, Learner)
+        assert isinstance(learner, Learner)
