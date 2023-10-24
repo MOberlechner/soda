571517c4f8aab407048f68422988a312acff9272
Matthias Oberlechner <matthias.oberlechner@tum.de>
2023-10-24 08:25:25 -0700

    update config for experiments

--- projects/ad_auctions/config_exp.py
+++ projects/ad_auctions/config_exp.py
@@ -1,2 +1,8 @@
-PATH_TO_CONFIGS = "projects/ad-auctions/configs/"
+PATH_TO_CONFIGS = "projects/ad_auctions/configs/"
 PATH_TO_EXPERIMENTS = "experiments/ad-auctions/"
+
+NUMBER_SAMPLES = int(2**22)
+ROUND_DECIMALS = 5
+
+LOGGING = True
+SAVE_STRAT = True
