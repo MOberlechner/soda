88b2cb6d98cd2c3a589315027f18a8ad9b277785
Matthias Oberlechner <matthias.oberlechner@tum.de>
2023-10-24 17:28:44 -0700

    include experiments for single-item auctions with interdependent values

---
+++ projects/soda/simulation/run_interdependent.py
@@ -0,0 +1,52 @@
+from itertools import product
+from time import time
+
+from projects.soda.config_exp import *
+from soda.util.experiment import Experiment
+
+game_affiliated = [
+    "interdependent/affiliated_values.yaml",
+]
+game_common = [
+    "interdependent/common_values_values.yaml",
+]
+learner_affiliated = [
+    "soda1_eta25_beta50.yaml",
+    "soda2_eta1_beta50.yaml",
+    "soma2_eta1_beta50.yaml",
+    "sofw.yaml",
+    "fp.yaml",
+]
+learner_common = [
+    "soda1_eta100_beta50.yaml",
+    "soda2_eta1_beta05.yaml",
+    "soma2_eta50_beta50.yaml",
+    "sofw.yaml",
+    "fp.yaml",
+]
+experiment_list = list(product(game_affiliated, learner_affiliated)) + list(
+    product(game_common, learner_common)
+)
+
+if __name__ == "__main__":
+    print(f"\nRunning {len(experiment_list)} Experiments".ljust(100, "."), "\n")
+    t0 = time()
+
+    for config_game, config_learner in experiment_list:
+
+        exp_handler = Experiment(
+            PATH_TO_CONFIGS + "game/" + config_game,
+            PATH_TO_CONFIGS + "learner/" + config_learner,
+            NUMBER_RUNS,
+            LEARNING,
+            SIMULATION,
+            LOGGING,
+            SAVE_STRAT,
+            NUMBER_SAMPLES,
+            SAVE_INIT_STRAT,
+            PATH_TO_EXPERIMENTS,
+            ROUND_DECIMALS,
+        )
+        exp_handler.run()
+    t1 = time()
+    print(f"\nExperiments finished ({(t1-t0)/60:.1f} min)".ljust(100, "."), "\n")
