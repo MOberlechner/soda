3b987a9cf5a8d142883b503d9e4a9de903d4048d
Matthias Oberlechner <matthias.oberlechner@tum.de>
2023-11-02 16:31:45 -0700

    update path for experiments

--- projects/ad_auctions/config_exp.py
+++ projects/ad_auctions/config_exp.py
@@ -1,5 +1,5 @@
 PATH_TO_CONFIGS = "projects/ad_auctions/configs/"
-PATH_TO_EXPERIMENTS = "experiments/ad-auctions/"
+PATH_TO_EXPERIMENTS = "experiments/ad_auctions/"
 
 NUMBER_SAMPLES = int(2**22)
 ROUND_DECIMALS = 5
