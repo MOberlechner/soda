8bc46026b6f78443aea5cd7c2477fc36c0ba1f66
Matthias Oberlechner <matthias.oberlechner@tum.de>
2023-11-03 16:09:15 -0700

    update info on ad-auction project

--- projects/ad_auctions/config_exp.py
+++ projects/ad_auctions/config_exp.py
@@ -1,5 +1,6 @@
 PATH_TO_CONFIGS = "projects/ad_auctions/configs/"
 PATH_TO_EXPERIMENTS = "experiments/ad_auctions/"
+PATH_SAVE = "projects/ad_auctions/results/"
 
 NUMBER_SAMPLES = int(2**22)
 ROUND_DECIMALS = 5
