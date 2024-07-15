3878fd03325b7850c6923d5a3a5b283e9a0dec5d
Matthias Oberlechner <matthias.oberlechner@tum.de>
2024-07-15 17:34:05 +0200

    update readme and configs

---
+++ projects/contests/config_exp.py
@@ -0,0 +1,28 @@
+# parameter to run experiments
+PATH_TO_CONFIGS = "projects/contests/configs/"
+PATH_SAVE = "projects/contests/results/"
+PATH_TO_EXPERIMENTS = "experiments/contests/"
+
+LEARNING = True
+
+NUMBER_RUNS = 10
+NUMBER_SAMPLES = int(2**22)
+
+LOGGING = True
+ROUND_DECIMALS = 5
+SAVE_INIT_STRAT = True
+SAVE_STRAT = True
+
+# parameter for evaluations
+ROUND_DECIMALS_TABLE = 3
+
+# Parameter plots
+DPI = 600
+FORMAT = "pdf"
+SAMPLES = 150
+MARKER = ["o", "s", "v"]
+MARKER_SIZE = 30
+COLORS = ["#003f5c", "#bc5090", "#ffa600"]
+FONTSIZE_LABEL = 14
+FONTSIZE_LEGEND = 15
+FONTSIZE_TITLE = 16
