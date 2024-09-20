00509bf33e0a2e491c693c54c1feb2e4cac28bf4
Matthias Oberlechner <matthias.oberlechner@tum.de>
2024-09-20 14:10:31 +0200

    update

--- projects/ad_auctions/config_exp.py
+++ projects/ad_auctions/config_exp.py
@@ -1,6 +1,6 @@
 PATH_TO_CONFIGS = "projects/ad_auctions/configs/"
-PATH_TO_EXPERIMENTS = "experiments/ad_auctions/"
-PATH_SAVE = "projects/ad_auctions/results/"
+PATH_TO_EXPERIMENTS = "experiments/ad_auction_isr/"
+PATH_SAVE = "projects/ad_auctions/results_isr/"
 
 NUMBER_SAMPLES = int(2**22)
 ROUND_DECIMALS = 5
