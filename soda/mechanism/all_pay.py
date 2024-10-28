5f39d8a2f6bd26095fa3d6c02564a8a19ccb2d3c
matthias <oberlechner@pm.me>
2024-10-28 18:03:43 +0100

    fixed error and renamed method get_metrics() to get_metrics_mechanism()

--- soda/mechanism/all_pay.py
+++ soda/mechanism/all_pay.py
@@ -167,17 +167,12 @@ class AllPayAuction(Mechanism):
 
     # --------------------------------- methods to compute metrics --------------------------------- #
 
-    def get_metrics(
+    def get_metrics_mechanism(
         self, agent: str, obs_profile: np.ndarray, bid_profile: np.ndarray
     ) -> tuple:
-        """compute all metrics relevant for all-pay auction
-        this includes standard metrics (l2, util_loss) + revenue
-        """
-        metrics, values = self.get_standard_metrics(agent, obs_profile, bid_profile)
-        metrics += ["revenue"]
-        values += [self.compute_expected_revenue(bid_profile)]
-
-        return metrics, values
+        """metric regarding mechanism (overwrites method from mechanism class)"""
+        rev = self.compute_expected_revenue(bid_profile)
+        return {"revenue": rev}
 
     def compute_expected_revenue(self, bid_profile: np.ndarray) -> float:
         """Computed expected revenue of all-pay auction
